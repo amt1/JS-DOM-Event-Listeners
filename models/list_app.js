197258754a97b89690b64426b746acf9381a01ab
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-04-07 20:01:55 +0100

    Javascript delete works and I've sorted out the flex box align to top issue

--- models/list_app.js
+++ models/list_app.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+// well that's a start
 
+const handleDeleteButtonClick = function() {
+  const eventList = document.querySelector('#events');
+  eventList.innerHTML='';
+};
 
+const clickDeleteButton = document.querySelector('#delete');
+clickDeleteButton.addEventListener('click',handleDeleteButtonClick);
 });
