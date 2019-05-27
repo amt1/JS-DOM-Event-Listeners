b9e1675def576d8157b31619c517c81a063019b3
amt1 <atreshansky@googlemail.com>
2019-05-27 16:17:58 +0100

    server added

---
+++ server.js
@@ -0,0 +1,65 @@
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+//    port = process.argv[2] || 8888;
+const port=process.env.PORT || 3000
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+
+  var contentTypesByExtension = {
+    '.html': "text/html",
+    '.css':  "text/css",
+    '.js':   "text/javascript"
+  };
+
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      var headers = {};
+      var contentType = contentTypesByExtension[path.extname(filename)];
+      if (contentType) headers["Content-Type"] = contentType;
+      response.writeHead(200, headers);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+
+
+// option 1 from stackoverflow:
+// var http = require('http');
+// var fs = require('fs');
+//
+// const PORT=8080;
+//
+// fs.readFile('./index.html', function (err, html) {
+//
+//     if (err) throw err;
+//
+//     http.createServer(function(request, response) {
+//         response.writeHeader(200, {"Content-Type": "text/html"});
+//         response.write(html);
+//         response.end();
+//     }).listen(PORT);
+// });
